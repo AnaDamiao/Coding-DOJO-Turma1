23e5f7c4d549da50f5fc62d892d0196d3cff9144
Victor Lopes <99993119+Victor-Lopes-Belo@users.noreply.github.com>
2024-11-12 19:56:46 -0300

    Add files via upload

---
+++ ALGORITIMO/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
